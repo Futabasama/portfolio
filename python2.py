862b41318479f8e9ab30cb13c49ba0638b0e0118
Futabasama <ahlamgarah1@gmail.com>
2026-05-09 15:13:06 +0100

    fix duplicate code

--- python2.py
+++
@@ -1,9 +0,0 @@
-def pgcd_recursif(a, b):
-    if b == 0:
-        return a
-    else:
-        return pgcd_recursif(b, a % b)
-# Tests
-print(f"pgcd_recursif(48, 18) = {pgcd_recursif(48, 18)}")
-print(f"pgcd_recursif(100, 35) = {pgcd_recursif(100, 35)}")
-print(f"pgcd_recursif(7, 5) = {pgcd_recursif(7, 5)}")
